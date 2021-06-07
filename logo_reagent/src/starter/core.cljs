2735dbe6c7aa51a372b5277c983f351ddaa372f6
Jarrett Atkinson <jarrettatkinson@freeformsoftware.dev>
2021-06-07 15:45:48 -0500

    * Logo Drafting table ideas

---
+++ logo_reagent/src/starter/core.cljs
@@ -0,0 +1,28 @@
+(ns starter.core
+    (:require [reagent.core :as r]))
+
+(defonce greeting "hello")
+(def who "world")
+
+(defn app []
+  [:svg {:style {:border "1px solid"
+                 :background "white"
+                 :width "150px"
+                 :height "150px"}}
+   [:circle {:r 50, :cx 75, :cy 75, :fill "green"}]
+   [:circle {:r 25, :cx 75, :cy 75, :fill "blue"}]
+   [:path {:stroke-width 12
+           :stroke "white"
+           :fill "none"
+           :d "M 30,40 C 100,40 50,110 120,110"}]])
+
+(defn stop []
+  (js/console.log "Stopping..."))
+
+(defn start []
+  (js/console.log "Starting...")
+  (r/render [app]
+            (.getElementById js/document "app")))
+
+(defn ^:export init []
+  (start))
